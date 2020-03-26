50dc5078f506f3e8b9e5c51671f580d1d6a045a8
wohfab <wohfab@gmail.com>
2020-03-26 21:31:54 +0100

    added img role to emoji for accesibility

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,7 +12,7 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Error 404 <span aria-label="see-no-evil emoji">🙈</span></h1>
+        <h1>Error 404 <span role="img" aria-label="see-no-evil emoji">🙈</span></h1>
         <p>This resource does not (yet) exit (at this place). If you need help, finding a page, consider, checking the Sitemap.</p>
         <p>Here are a few awesome pages on this website, you might be interested in:</p>
         <ul>
