77825b1d7b8930838a6bcce807cce4d05d60376f
wohfab <wohfab@gmail.com>
2020-03-26 21:27:55 +0100

    edit 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,8 +12,18 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>Error 404 <span aria-label="see-no-evil emoji">🙈</span></h1>
+        <p>This resource does not (yet) exit (at this place). If you need help, finding a page, consider, checking the Sitemap.</p>
+        <p>Here are a few awesome pages on this website, you might be interested in:</p>
+        <ul>
+          <li><a href="/">Start</a> - the start page</li>
+          <li><a href="/blog">Blog</a> - overview of the blog posts</li>
+        </ul>
+        <p>And here are a few external resources, you might want to check out:</p>
+        <ul>
+        <li><a href="https://twitter.com">Twitter</a> - because people live there</li>
+        <li><a href="https://reddit.com">Reddit</a> - because people <em>actually</em> live there</li>
+        </ul>
       </Layout>
     )
   }
