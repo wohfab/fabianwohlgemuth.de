c5e9a1bf82aee1e3b130efcf870e81fe78c9655d
wohfab <wohfab@gmail.com>
2020-03-27 08:44:20 +0100

    Made TYPO custom

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,23 +1,19 @@
+
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    "Oswald",
+    "Courier New",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Georgia", "serif"],
+})
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
-
-// Hot reload typography in development.
-if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
-}
-
+export const { scale, rhythm, options } = typography
 export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
