881ab81809d9e05bff739a5e18ac815190ad2f1f
wohfab <wohfab@gmail.com>
2020-03-31 15:55:22 +0200

    add graphql import

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StaticQuery } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import { rhythm } from "../utils/typography"
 
