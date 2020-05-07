b24aba91fb9286134a868ba389d9501633806a1d
wohfab <wohfab@gmail.com>
2020-05-07 10:22:36 +0200

    fixed casing

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { Link, graphql } from "gatsby"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 
 class CategoryRoute extends React.Component {
   render() {
